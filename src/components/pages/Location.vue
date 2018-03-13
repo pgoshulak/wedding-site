bafc76c635c0f6f0beac7dbeb360f465dc7a29e8
Peter Goshulak <pgoshulak@gmail.com>
2018-03-13 16:43:02 -0400

    Add 'directions' link

--- src/components/pages/Location.vue
+++ src/components/pages/Location.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="home">
     <h3 class="md-display-1">Location</h3>
-    <p>Steph and Peter will be married at <em>Earth to Table: The Farm</em>, near Hamilton ON.</p>
+    <p>Steph and Peter will be married at <em>Earth to Table: The Farm </em>
+      <a :href="directionsHref" target="_blank">(Directions)</a>
+      , near Hamilton ON.</p>
     <p>The ceremony begins at 4:00pm, with reception and dinner to follow</p>
     <iframe :src="src" frameborder="0" allowfullscreen></iframe>
   </div>
@@ -9,12 +11,13 @@
 
 <script>
   const API_KEY = 'AIzaSyAYI6g6LhzhqAQVnRxCyzoz_N-aWVh45xw'
-  const MAP_QUERY = `Earth+to+Table+The+Farm,Millgrove+ON`
+  const MAP_QUERY = `Earth%20to%20Table%3A%20The%20Farm%2C%20Millgrove%20ON`
   const MAP_ZOOM = 11
   export default {
     name: 'Location',
     computed: {
-      src () { return `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${MAP_QUERY}&zoom=${MAP_ZOOM}` }
+      src () { return `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${MAP_QUERY}&zoom=${MAP_ZOOM}` },
+      directionsHref () { return `https://www.google.com/maps/dir/?api=1&destination=${MAP_QUERY}` }
     }
   }
 </script>
@@ -24,5 +27,8 @@ iframe {
   width: 100%;
   min-height: 400px;
   border: 0
+}
+a {
+  font-weight: bold;
 }
 </style>
