4ecbc91c48d622cc99e7c76ed5168264a943860c
Peter Goshulak <pgoshulak@gmail.com>
2018-03-13 11:15:01 -0400

    Add google maps iframe to Location

--- src/components/pages/Location.vue
+++ src/components/pages/Location.vue
@@ -2,15 +2,26 @@
   <div id="home">
     <h3 class="md-display-1">Location</h3>
     <p>Steph and Peter will be married at <em>Earth to Table: The Farm</em>, near Hamilton ON.</p>
+    <iframe :src="src" frameborder="0" allowfullscreen></iframe>
   </div>
 </template>
 
 <script>
+  const API_KEY = 'AIzaSyAYI6g6LhzhqAQVnRxCyzoz_N-aWVh45xw'
+  const MAP_QUERY = `Earth+to+Table+The+Farm,Millgrove+ON`
+  const MAP_ZOOM = 13
   export default {
-    name: 'Location'
+    name: 'Location',
+    computed: {
+      src () { return `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${MAP_QUERY}&zoom=${MAP_ZOOM}` }
+    }
   }
 </script>
 
 <style>
-
+iframe {
+  width: 100%;
+  min-height: 400px;
+  border: 0
+}
 </style>
