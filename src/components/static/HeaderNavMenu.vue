d923d45b65c057f7ec072c3f209bdc2d4ec2aff1
Peter Goshulak <pgoshulak@gmail.com>
2018-03-18 17:53:14 -0400

    Fix NavMenu to close after link is clicked

--- src/components/static/HeaderNavMenu.vue
+++ src/components/static/HeaderNavMenu.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <md-menu id="nav-menu">
+    <md-menu id="nav-menu" :md-active.sync="toggleMenu">
       <md-button id="nav-menu-button" class="md-raised md-primary md-icon-button" md-menu-trigger>
         <md-icon>menu</md-icon>
       </md-button>
 
       <md-menu-content>
-        <md-menu-item v-for="link in links" :key="link.id" :to="link.to">
+        <md-menu-item v-for="link in links" :key="link.id" :to="link.to" @click="toggleMenu=false">
           <span class="md-list-item-text">{{link.label}}</span>
         </md-menu-item>
       </md-menu-content>
@@ -17,7 +17,12 @@
 
 <script>
   export default {
-    name: 'HeaderNavDrawer',
+    name: 'HeaderNavMenu',
+    data () {
+      return {
+        toggleMenu: false
+      }
+    },
     props: ['links']
   }
 </script>
