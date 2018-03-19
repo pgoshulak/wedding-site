1f5da5caf5467e2e07e74478429d3809e89050c3
Peter Goshulak <pgoshulak@gmail.com>
2018-03-19 17:39:38 -0400

    Add photo url fetch from firestore

--- src/components/pages/Photos.vue
+++ src/components/pages/Photos.vue
@@ -1,35 +1,45 @@
 <template>
   <div id="home">
-    <h3 class="md-display-1">Photos</h3>
-    <!-- <md-button class="md-accent">hi there</md-button> -->
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
+    <h3 class="md-display-1">Photo Gallery</h3>
+    <img :src="photo.thumbUrl" :key="photo.uploaded" alt="image" v-for="photo in photoList">
   </div>
 </template>
 
 <script>
+  import {apiKey as API_KEY} from '../../../service/firebase.js'
+  import firebase from 'firebase'
+  import 'firebase/firestore'
+
   export default {
-    name: 'Home'
+    name: 'Photos',
+    data () {
+      return {
+        // db: null,
+        photoList: []
+      }
+    },
+    methods: {
+      getPhotos (db) {
+        let photos = []
+        db.collection('photos').get().then((snap) => {
+          snap.forEach((doc) => {
+            photos.push(doc.data())
+          })
+        })
+        return photos
+      }
+    },
+    created () {
+      if (!firebase.apps.length) {
+        firebase.initializeApp({
+          apiKey: API_KEY,
+          authDomain: 'peeph-wedding.firebaseapp.com',
+          projectId: 'peeph-wedding'
+        })
+      }
+      let db = firebase.firestore()
+      this.photoList = this.getPhotos(db)
+    }
   }
 </script>
 
