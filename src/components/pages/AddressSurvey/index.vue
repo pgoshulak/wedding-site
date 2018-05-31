d6bca6c446549182f19202c998006c4973e15458
Peter Goshulak <pgoshulak@gmail.com>
2018-05-31 09:08:58 -0400

    Add search-by-email functionality

--- src/components/pages/AddressSurvey/index.vue
+++ src/components/pages/AddressSurvey/index.vue
@@ -2,22 +2,62 @@
   <div id="home">
     <h3 class="md-display-1">Address Survey</h3>
     <SearchArea v-if="searchResults.length==0" @submitSearch="submitSearch"></SearchArea>
-
+    <pre>{{searchResults}}</pre>
   </div>
 </template>
 
 <script>
+  import {apiKey as API_KEY} from '../../../../service/firebase.js'
+  import * as firebase from 'firebase'
+  import 'firebase/firestore'
   import SearchArea from './SearchArea'
   export default {
     data () {
       return {
-        searchResults: []
+        db: null,
+        guestsRef: null,
+        searchTerm: '',
+        searchResults: [],
+        showSnackbarNoResults: false
       }
     },
     methods: {
-      submitSearch (data) {
-        console.log(data)
+      submitSearch (searchTerm) {
+        // console.log(data)
+        this.guestsRef = this.guestsRef || this.db.collection('guests')
+        this.searchTerm = searchTerm
+        this.searchResults = []
+        this.guestsRef.where('email', '==', searchTerm).get()
+          .then(snap => {
+            if (snap.empty) {
+              this.showSnackbarNoResults = true
+            }
+            snap.forEach(docRef => {
+              this.searchResults.push({
+                id: docRef.id,
+                data: docRef.data()
+              })
+            })
+          }).catch(err => {
+            console.error('Error searching for ' + searchTerm, err)
+          })
       }
+    },
+    created () {
+      console.log('inside created()')
+      console.log(firebase)
+      if (!firebase.apps.length) {
+        console.log('initializing firebase')
+        firebase.initializeApp({
+          apiKey: API_KEY,
+          authDomain: 'peeph-wedding.firebaseapp.com',
+          projectId: 'peeph-wedding'
+        })
+      }
+      console.log('setting this.db = firebase.firestore()')
+      this.db = firebase.firestore().settings({
+        timestampsInSnapshots: true
+      })
     },
     components: {
       SearchArea
