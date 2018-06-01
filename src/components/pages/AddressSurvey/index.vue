b49a11cca5163cf316e6c670859126d3adaad7db
Peter Goshulak <pgoshulak@gmail.com>
2018-06-01 11:10:31 -0400

    Update email search to return family data and all members

--- src/components/pages/AddressSurvey/index.vue
+++ src/components/pages/AddressSurvey/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="home">
     <h3 class="md-display-1">Address Survey</h3>
-    <SearchArea v-if="searchResults.length==0" @submitSearch="submitSearch"></SearchArea>
-    <pre>{{searchResults}}</pre>
+    <SearchArea v-if="searchResultsGuests.length==0" @submitSearch="submitSearch"></SearchArea>
+    <pre>{{searchResultsFamily}}</pre>
+    <pre>{{searchResultsGuests}}</pre>
   </div>
 </template>
 
@@ -13,27 +14,43 @@
     data () {
       return {
         guestsRef: null,
+        familiesRef: null,
         searchTerm: '',
-        searchResults: [],
+        searchResultsFamily: {},
+        searchResultsGuests: [],
         showSnackbarNoResults: false
       }
     },
     methods: {
       submitSearch (searchTerm) {
-        // console.log(data)
         this.searchTerm = searchTerm
-        this.searchResults = []
+        this.searchResultsFamily = {}
+        this.searchResultsGuests = []
+
+        // Fetch the single guest matching the email address
         this.guestsRef.where('email', '==', searchTerm).get()
-          .then(snap => {
-            if (snap.empty) {
+          .then(snap1 => {
+            // No results
+            if (snap1.empty) {
               this.showSnackbarNoResults = true
+            } else {
+              // Retrieve the guest's familyId
+              const familyId = snap1.docs[0].data().familyId
+              // Query for all guests in the family
+              this.guestsRef.where('familyId', '==', familyId).get()
+                .then(snap2 => {
+                  // Add each guest of the family into the results array
+                  snap2.forEach(guestRef => {
+                    this.searchResultsGuests.push(guestRef.data())
+                  })
+                })
+              // Query for the family's data (address, etc)
+              this.familiesRef.doc(familyId).get()
+                .then(snap3 => {
+                  console.log(snap3)
+                  this.searchResultsFamily = snap3.data()
+                })
             }
-            snap.forEach(docRef => {
-              this.searchResults.push({
-                id: docRef.id,
-                data: docRef.data()
-              })
-            })
           }).catch(err => {
             console.error('Error searching for ' + searchTerm, err)
           })
@@ -41,6 +58,7 @@
     },
     created () {
       this.guestsRef = db.collection('guests')
+      this.familiesRef = db.collection('families')
     },
     components: {
       SearchArea
