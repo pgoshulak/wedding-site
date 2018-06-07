0913ddfbac7e0a40840d0bb285e831ffc57cbd9d
Peter Goshulak <pgoshulak@gmail.com>
2018-06-07 19:57:47 -0400

    Fix <form> breaking flexbox layout

--- src/components/pages/AddressSurvey/FamilyData.vue
+++ src/components/pages/AddressSurvey/FamilyData.vue
@@ -1,40 +1,40 @@
 <template>
   <div>
-    <div class="md-layout md-gutter md-alignment-center-left">
-      <form action="#" autocomplete="on">
+    <form action="#" autocomplete="on">
+      <div class="md-layout md-gutter md-alignment-center-left">
 
-        <div class="md-layout-item md-size-80 md-xsmall-size-100">
+        <div class="md-layout-item md-size-100 md-xsmall-size-100">
           <md-field :md-counter="false">
             <label for="streetAddress">Street Address</label>
             <md-input id="streetAddress" v-model="streetAddress" @change="registerUnsavedChange"></md-input>
           </md-field>
         </div>
-        <div class="md-layout-item md-size-45 md-xsmall-size-100">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
           <md-field :md-counter="false">
             <label for="city">City</label>
             <md-input id="city" v-model="city" @change="registerUnsavedChange"></md-input>
           </md-field>
         </div>
-        <div class="md-layout-item md-size-45 md-xsmall-size-100">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
           <md-field :md-counter="false">
             <label for="province">Province/State</label>
             <md-input id="province" v-model="province" @change="registerUnsavedChange"></md-input>
           </md-field>
         </div>
-        <div class="md-layout-item md-size-45 md-xsmall-size-100">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
           <md-field :md-counter="false">
             <label for="postalCode">Postal/Zip Code</label>
             <md-input id="postalCode" v-model="postalCode" @change="registerUnsavedChange"></md-input>
           </md-field>
         </div>
-        <div class="md-layout-item md-size-45 md-xsmall-size-100">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
           <md-field :md-counter="false">
             <label for="country">Country</label>
             <md-input id="country" v-model="country" @change="registerUnsavedChange"></md-input>
           </md-field>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -92,12 +92,5 @@
 </script>
 
 <style scoped>
-  /* FIXME: <form> forces all fields onto separate lines */
-  form {
-    width: 100%;
-    padding: 10px;
-  }
-  form .md-field {
-    margin: 4px 0 8px;
-  }
+
 </style>
