acfab0427169319b4d0e5231beb87303cfe755f9
Peter Goshulak <pgoshulak@gmail.com>
2018-08-18 20:37:26 -0400

    Add Our-Story

--- src/components/pages/OurStory.vue
+++ src/components/pages/OurStory.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="home">
     <h3 class="md-display-2">Our Story</h3>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
-    <p>some text here</p>
+    <p>
+      Steph and Peter began their fairy-tale love story in September 2014 at the Toronto Rehabilitation Institute - she, a responsible speech therapist on the Brain Injury floor - he, a gangly student volunteer with a weird Batman obsession. They chatted as friends and co-workers every week when Peter volunteered in the “Baking Group” rehab sessions, and Peter impressed Steph with his ability to run an organized kitchen. Clearly, this weirdo could cook.
+    </p><p>
+      After Peter left TRI and joined the workforce as an adult (lol), they ran into each other on the streets of downtown Toronto every few months. Peter made several moves, including boldly handing over his business card while saying “You should call me”, but Steph was playing cool-girl-who-doesn’t-call-back, and Peter was stumped.
+    </p><p>
+      It wasn’t until a rainy day in August 2016 when they met at University and Dundas. Peter walked Steph towards her car under his umbrella, and he worked up the courage to ask her out later that night. This time, Steph played cool-girl-who-accepts, and on their first date they discussed their passions for juggling (Steph) and magic tricks (Peter). Clearly, these two dorks were meant for each other.
+    </p><p>
+      Fast-forward through 15 months of acro-yoga, puns, and accidentally-matched outfits to December 15 2017, where Steph and Peter are vacationing in Hawaii with their Best Frands (aka. Passion Trio and Rojo). Peter tricks Steph into some acro-yoga at Shipwreck's Beach, then gets down on one knee exactly at sunset (how precisely planned!). She gasped in incredulity, said yes, and … well, now we want YOU to help us celebrate!
+    </p>
+
   </div>
 </template>
 
